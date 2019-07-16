ceb04321ce768a85ae36abd62c0c1d9bb4ee56bd
shikugawa <shikugawa@gmail.com>
2019-07-17 00:25:18 +0900

    fix path resolver in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
+const path = require('path');
+
 module.exports = {
     mode: "production",
     entry: "./src/index.js",
     output: {
-        path: __dirname + '/dist',
+        path: path.resolve(__dirname, 'dist'),
         filename: 'ingestly.js'
     },
     module: {
